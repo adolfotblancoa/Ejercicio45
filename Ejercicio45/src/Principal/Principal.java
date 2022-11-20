7e95270218113575a5704cb17edfebf0615be4e1
adolfotblancoa <115952570+adolfotblancoa@users.noreply.github.com>
2022-11-20 23:08:29 +0100

    Add files via upload

---
+++ Ejercicio45/src/Principal/Principal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Principal{
+public static void main(String []args){
+        int a, b, suma;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca un numero: ");
+        a = teclado.nextInt();
+        System.out.println("Introduzca otro numero: ");
+        b = teclado.nextInt();
+        suma = a + b;
+        System.out.println("La suma de ambos numeros es: " + suma);
+}
+}
+
+
